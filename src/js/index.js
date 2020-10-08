52c0264cb115c4911445b3a3cc72a8de85b3d135
amp9020 <amp9020@yahoo.com>
2020-10-08 13:45:28 -0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js");
+
+  navigator.serviceWorker.ready.then((registration) => {
+    if (registration.sync) {
+      registration.sync.register("apiSync");
+    }
+  });
+}
